3ea09c5b35f79e88ff49a5088b3929185facd636
Move21Style <move21style@googlemail.com>
2017-08-02 20:31:08 +0200

    Added test case to check word were read coorectly

--- SpeedyKey/src/java/test/com/mmi/util/FileUtilTest.java
+++ SpeedyKey/src/java/test/com/mmi/util/FileUtilTest.java
@@ -2,9 +2,8 @@ package com.mmi.util;
 
 import java.util.List;
 
-import org.junit.Test;
-
 import org.junit.Assert;
+import org.junit.Test;
 
 public class FileUtilTest {
 
@@ -13,6 +12,18 @@ public class FileUtilTest {
 		List<String> availableTxtFiles = FileUtil.getAvailableTxtFiles();
 		Assert.assertEquals("Unexpected number txt files found", 1, availableTxtFiles.size());
 		Assert.assertEquals("Unexpected file name", "leseprobe.txt", availableTxtFiles.get(0));
+	}
+
+	@Test
+	public void testReadWords() throws Exception {
+		List<String> words = FileUtil.readWords("leseprobe.txt");
+		Assert.assertEquals("Unexpected number words", 318, words.size());
+
+		for (String word : words) {
+			Assert.assertTrue("Empty word entry <" + words.indexOf(word) + ">",
+					word != null && word.trim().length() > 0);
+		}
+		System.out.println(words.size());
 	}
 
 }
