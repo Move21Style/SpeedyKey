16969c08558b77da52100c7636ed8da19a2474d2
Move21Style <move21style@googlemail.com>
2017-08-07 15:05:05 +0200

    Check out keyTyped. Seems to fit my requirements.

--- SpeedyKey/src/java/impl/com/mmi/listener/SpeedyKeyListener.java
+++ SpeedyKey/src/java/impl/com/mmi/listener/SpeedyKeyListener.java
@@ -23,7 +23,29 @@ public class SpeedyKeyListener extends KeyAdapter {
 		boolean altDown = e.isAltDown();
 		String keyText = KeyEvent.getKeyText(e.getKeyCode());
 
-		System.out.println("Input" //
+		System.out.println("keyPressed(KeyEvent e)" //
+				+ ": keyCode=" + keyCode //
+				+ ", keyChar=" + keyChar //
+				+ ", modifiersEx=" + modifiersEx //
+				+ ", shiftDown=" + shiftDown //
+				+ ", extendedKeyCode=" + extendedKeyCode //
+				+ ", altDown=" + altDown //
+				+ ", keyText=" + keyText);
+	}
+
+	// TODO mmi: 
+	//		----> Seems to be the solution <----
+	@Override
+	public void keyTyped(KeyEvent e) {
+		int keyCode = e.getKeyCode();
+		char keyChar = e.getKeyChar();
+		int modifiersEx = e.getModifiersEx();
+		boolean shiftDown = e.isShiftDown();
+		int extendedKeyCode = e.getExtendedKeyCode();
+		boolean altDown = e.isAltDown();
+		String keyText = KeyEvent.getKeyText(e.getKeyCode());
+
+		System.out.println("keyTyped(KeyEvent e)" //
 				+ ": keyCode=" + keyCode //
 				+ ", keyChar=" + keyChar //
 				+ ", modifiersEx=" + modifiersEx //
